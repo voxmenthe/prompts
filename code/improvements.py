11da31a1faa9b13b5884e151da90fe5fd2dc4e1c
Jeff Coggshall <deeplearnjeff@gmail.com>
2024-09-20 20:21:38 -0700

    personas

---
+++ code/improvements.py
@@ -0,0 +1,11 @@
+uplevel1 = """
+Always add a few comments highlighting the following:
+
+1) What are other ways we could write this code?  What are the pros and cons of those approaches?
+
+2) Explain this code from the perspective of {persona}.  What things are easier and what is harder?
+
+3) Are there ways of refactoring this code to be more modular, readable, and robust?
+
+4) What are common mistakes people make when writing code like this?  Show examples.
+"""
---
+++ code/personas.py
@@ -0,0 +1,4 @@
+coder_roles = {
+    "python1": "an experienced Python programmer",
+    "code_ninja1": "a guy who won several coding competitions and hackathons",
+}
