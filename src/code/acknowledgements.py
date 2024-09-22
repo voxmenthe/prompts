111c173589cb0cef22bef2fc60b7bd653282db7a
Jeff Coggshall <deeplearnjeff@gmail.com>
2024-09-21 20:39:22 -0700

    shorten acknowledgement

--- src/code/acknowledgements.py
+++ src/code/acknowledgements.py
@@ -1,10 +1,18 @@
-acknowledgements = """Just to make sure that I know you've incorporated these instructions, please respond with any of the following emojis as appropriate at the very end of your response:
+acknowledgement = """Just to make sure that I know you've incorporated these instructions, please respond with any of the following emojis as appropriate at the very end of your response:
 👀 (Eyes) - Indicating "I've seen/read this"
 ✅ (Check Mark) - Signifying "Understood" or "Noted"
 🫡 (Saluting Face) - Suggesting "Acknowledged" or "Roger that"
 📚 (Books) - Implying "Information processed"
 💡 (Light Bulb) - Indicating "I've grasped this concept"
 🔄 (Recycling Symbol) - Suggesting "I'll keep this in mind"
+🤔 (Thinking Face) - Indicating "I've considered this"
+🧠 (Brain) - Indicating "I've included a substantial amount of additional appropriate information"
+"""
+
+acknowledgement_short = """Just to make sure that I know you've incorporated these instructions, please respond with at least one of the following emojis as appropriate at the very end of your response:
+
+✅ (Check Mark) - Signifying "Understood" or "Noted"
+💡 (Light Bulb) - Indicating "I've grasped this concept"
 🤔 (Thinking Face) - Indicating "I've considered this"
 🧠 (Brain) - Indicating "I've included a substantial amount of additional appropriate information"
 """
