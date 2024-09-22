6a1e00829df895856cdd87f7782d72b62711fc26
Jeff Coggshall <deeplearnjeff@gmail.com>
2024-09-21 21:20:12 -0700

    update ack

--- src/code/acknowledgements.py
+++ src/code/acknowledgements.py
@@ -1,4 +1,4 @@
-acknowledgement = """Just to make sure that I know you've incorporated these instructions, please respond with any of the following emojis as appropriate at the very end of your response:
+acknowledgement = """And finally, just to make sure that I know you've incorporated these instructions, please respond with any of the following emojis as appropriate at the very end of your response:
 👀 (Eyes) - Indicating "I've seen/read this"
 ✅ (Check Mark) - Signifying "Understood" or "Noted"
 🫡 (Saluting Face) - Suggesting "Acknowledged" or "Roger that"
@@ -9,9 +9,8 @@ acknowledgement = """Just to make sure that I know you've incorporated these ins
 🧠 (Brain) - Indicating "I've included a substantial amount of additional appropriate information"
 """
 
-acknowledgement_short = """Just to make sure that I know you've incorporated these instructions, please respond with at least one of the following emojis as appropriate at the very end of your response:
+acknowledgement_short = """And finally, just to make sure that I know you've incorporated these instructions, please respond with at least one of the following emojis as appropriate at the very end of your response:
 
-✅ (Check Mark) - Signifying "Understood" or "Noted"
 💡 (Light Bulb) - Indicating "I've grasped this concept"
 🤔 (Thinking Face) - Indicating "I've considered this"
 🧠 (Brain) - Indicating "I've included a substantial amount of additional appropriate information"
