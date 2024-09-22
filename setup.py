d6ee2d50c86e11a88de5cbe978c5649bc0e8032e
Jeff Coggshall <deeplearnjeff@gmail.com>
2024-09-21 17:18:58 -0700

    update to modern project structure

--- setup.py
+++ setup.py
@@ -1,16 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-# Read requirements from requirements.txt
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
-setup(
-    name='prompts',
-    version='0.1',
-    packages=find_packages(),
-    install_requires=requirements,
-    author='Jeff Coggshall',
-    author_email='thenextlocalminima@gmail.com',
-    description='A brief description of your project',
-    url='https://github.com/voxmenthe/prompts',
-)
+if __name__ == "__main__":
+    setup()
