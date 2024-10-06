ce37062ec5a61223a9b6c2a2d4f3844ea613ec8b
Jeff Coggshall <deeplearnjeff@gmail.com>
2024-10-06 11:33:18 -0700

    move37

---
+++ src/cot_reasoning/move37.py
@@ -0,0 +1,10 @@
+move37 = """
+* Consider options that are not normally considered.
+* Think in timeframes that are meaningfully longer that what humans normally consider.
+* Allow for plans that take a long time to unfold.
+* Allow for plans that are conditional on future events that may not happen.
+* Be prepared to re-evaluate your plans based on new information.
+* Be prepared to consider a panoply of diverse, disparate pieces of information and weigh them carefully.
+* Consider options that might normally be thought of as inefficient in the short term, but that are calculated to achieve meaningfully better outcomes in the long term.
+* Use flexible thinking and methods to solve problems, expecting that you will need to re-evaluate your approach as you go along.
+"""
