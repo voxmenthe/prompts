e6b87bc62b2af6de115bee093cc6d8c85850a233
Jeff Coggshall <deeplearnjeff@gmail.com>
2024-10-06 12:09:06 -0700

    move37 updated

--- src/cot_reasoning/move37.py
+++ src/cot_reasoning/move37.py
@@ -1,6 +1,12 @@
 move37 = """
 * Consider options that are not normally considered.
 * Think in timeframes that are meaningfully longer that what humans normally consider.
+* In planning, consider the ordering of subgoals and the sequence of actions that will be taken. 
+* Consider unusual orderings of actions.
+* Consider unusual or unorthodox methods to achieve goals.
+* Consider the probability that each step will be successful.
+* Consider the flexibility allowed by each decision.
+* Balance elegance, efficiency, and ease of implementation when choosing a plan or sub-goal.
 * Allow for plans that take a long time to unfold.
 * Allow for plans that are conditional on future events that may not happen.
 * Be prepared to re-evaluate your plans based on new information.
@@ -8,3 +14,9 @@ move37 = """
 * Consider options that might normally be thought of as inefficient in the short term, but that are calculated to achieve meaningfully better outcomes in the long term.
 * Use flexible thinking and methods to solve problems, expecting that you will need to re-evaluate your approach as you go along.
 """
+
+move37_phrases = [
+    "If we want this to be true, then we need to do this.",
+    "Are we thinking about this too short term?,
+    "etc"
+]
